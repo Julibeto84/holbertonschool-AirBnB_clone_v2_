18717a2cb9cbad1efb01403dd50fa6bca0e2d749
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-12-12 13:41:17 -0500

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,3 +9,6 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False) #the route() decorator to tell Flask what URL should trigger our function
 def hello_world():
     return("Hello HBNB!")
+
+if __name__ == "__main__":
+    app.run("host=0.0.0.0", port=5000)
