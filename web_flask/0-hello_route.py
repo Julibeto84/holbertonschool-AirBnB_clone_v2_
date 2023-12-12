095f8a443d7ef2dcaf6de8aace1c897bdbc7ff06
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-12-12 13:28:31 -0500

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+"""This module uses strict_slashes=False in the path definition."""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,5 +9,3 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False) #the route() decorator to tell Flask what URL should trigger our function
 def hello_world():
     return("Hello HBNB!")
-
-
