5fc84aab3dfdf7154e4e3623c4bc0fd6b685fd18
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-12-18 09:41:44 -0500

    updete commits

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,46 @@
+#!/usr/bin/python3
+
+
+"""
+The module creates a script that starts a Flask web application
+"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+# We use route() decorator to tell Flask which URL should trigger our function
+def first_route():
+    return'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def define_route():
+    return'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def print_text(text):
+    formatted_text = text.replace('_', ' ')
+    return f'C {escape(formatted_text)}'
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def print_text_py(text):
+    formatted_text = text.replace('_', ' ')
+    return f'Python {escape(formatted_text)}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
